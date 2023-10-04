f43edd2bcaebaff962d98af884b0b13f06fab337
NikolasMenezes <111708104+NikolasMenezes@users.noreply.github.com>
2023-10-03 21:51:15 -0300

    Add files via upload

---
+++ v-for/src/App.vue
@@ -0,0 +1,63 @@
+<script>
+import Card from './components/Card.vue'
+export default {
+  components:{
+    Card
+  },  
+  data (){
+    return {
+      array: [
+        {
+          id: 1,
+          title: "bncuoasncsa",
+          name: "Sofia",
+          idade: 20,
+          color: "rainbow"
+        },
+        {
+          id: 2,
+          title: "bncuoasncsa",
+          name: "Thauan",
+          idade: 240,
+          color: "rainbow"
+        },
+        {
+          id: 3,
+          title: "Tahuan bobão",
+          name: "Sia",
+          idade: 20,
+          color: "rainbow"
+        },
+        {
+          id: 4,
+          title: "titulo",
+          name: "Sascnaia",
+          idade: 721,
+          color: "rainbow"
+        }
+      ]
+    }
+  } 
+}
+</script>
+
+<template>
+
+<div class="cards-container">
+    <Card
+      v-for="item in array"
+      :key="item.id"
+      :title="item.title"
+      :name="item.name"
+      :idade="item.idade"
+    />
+  </div>
+
+</template>
+
+<style scoped>
+  .cards-container{
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
+</style>
---
+++ v-for/src/components/Card.vue
@@ -0,0 +1,38 @@
+<script>
+
+export default {
+  props: {
+    title: String,
+    name: String,
+    idade: Number,
+    color: String
+  }
+}
+
+</script>
+
+<template>
+  <div class="card">
+    <h1 class="card-title">
+      {{ title }}
+    </h1>
+    <p>Parabéns, {{ name }}, você tem {{ idade }} anos!</p>
+  </div>
+</template>
+
+<style scoped>
+
+  .card{
+    background-color: blue;
+    color: #f1f1f1;
+    border: 1px solid #000;
+    border-radius: 20px;
+    width: 200px;
+    height: 200px;
+    text-align: center;
+    font-size: 20px;
+    margin: 10px;
+    padding: 40px;
+  }
+
+</style>
